113776436c31667e115cd9fbfd5ad723369dc9c2
Alexander Sharov <kvendingoldo@yandex.ru>
2024-01-11 17:44:11 +0100

    feat: add basic CLI layout for tenv

--- cmd/tofu/tofu.tf.go
+++ cmd/tofu/tofu.tf.go
@@ -15,17 +15,4 @@
  * limitations under the License.
  *
  */
-package env
-
-import "os"
-
-const (
-	RootEnv = "TENV_ROOT_PATH"
-)
-
-func GetEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
-	}
-	return fallback
-}
+package main
