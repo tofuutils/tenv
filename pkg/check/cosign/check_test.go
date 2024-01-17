698347677ed09b55a9b7686080f5295259952f7b
Denis Vaumoron <dvaumoron@gmail.com>
2024-01-17 11:34:56 +0100

    add cosigncheck test

---
+++ pkg/check/cosign/check_test.go
@@ -0,0 +1,53 @@
+/*
+ *
+ * Copyright 2024 gotofuenv authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package cosigncheck_test
+
+import (
+	_ "embed"
+	"testing"
+
+	cosigncheck "github.com/dvaumoron/gotofuenv/pkg/check/cosign"
+)
+
+//go:embed tofu_1.6.0_arm64.deb
+var data []byte
+
+//go:embed tofu_1.6.0_arm64.deb.sig
+var dataSig []byte
+
+//go:embed tofu_1.6.0_arm64.deb.pem
+var dataKey []byte
+
+func TestCosignCheckCorrect(t *testing.T) {
+	if err := cosigncheck.Check(data, dataSig, dataKey); err != nil {
+		t.Error("Unexpected error : ", err)
+	}
+}
+
+func TestCosignCheckErrorKey(t *testing.T) {
+	if err := cosigncheck.Check(data, dataSig, dataKey[1:]); err == nil {
+		t.Error("Should fail on erroneus public key")
+	}
+}
+
+func TestCosignCheckErrorSig(t *testing.T) {
+	if err := cosigncheck.Check(data, dataSig[1:], dataKey); err == nil {
+		t.Error("Should fail on erroneus signature")
+	}
+}
