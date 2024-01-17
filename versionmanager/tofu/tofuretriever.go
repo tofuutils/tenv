ca89a3a5ea6c63ccfc29e0ab8fbad1d41513ef7d
Denis Vaumoron <dvaumoron@gmail.com>
2024-01-17 18:40:56 +0100

    add check cosign signature

--- versionmanager/tofu/tofuretriever.go
+++ versionmanager/tofu/tofuretriever.go
@@ -19,18 +19,25 @@
 package tofuretriever
 
 import (
+	"fmt"
 	"runtime"
 	"strings"
 
 	"github.com/dvaumoron/gotofuenv/config"
+	cosigncheck "github.com/dvaumoron/gotofuenv/pkg/check/cosign"
 	pgpcheck "github.com/dvaumoron/gotofuenv/pkg/check/pgp"
 	sha256check "github.com/dvaumoron/gotofuenv/pkg/check/sha256"
 	"github.com/dvaumoron/gotofuenv/pkg/download"
 	"github.com/dvaumoron/gotofuenv/pkg/github"
-	"github.com/dvaumoron/gotofuenv/versionmanager/semantic"
+	"github.com/hashicorp/go-version"
 )
 
 const publicKeyUrl = "https://get.opentofu.org/opentofu.asc"
+
+const (
+	baseIdentity = "https://github.com/opentofu/opentofu/.github/workflows/release.yml@refs/heads/v"
+	issuer       = "https://token.actions.githubusercontent.com"
+)
 
 type TofuRetriever struct {
 	conf *config.Config
@@ -40,18 +47,23 @@ func NewTofuRetriever(conf *config.Config) *TofuRetriever {
 	return &TofuRetriever{conf: conf}
 }
 
-func (r *TofuRetriever) DownloadReleaseZip(version string) ([]byte, error) {
-	tag := version
+func (r *TofuRetriever) DownloadReleaseZip(versionStr string) ([]byte, error) {
+	tag := versionStr
 	// assume that opentofu tags start with a 'v'
 	// and version in asset name does not
 	if tag[0] == 'v' {
-		version = version[1:]
+		versionStr = versionStr[1:]
 	} else {
-		tag = "v" + version
+		tag = "v" + versionStr
 	}
 
-	stable := semantic.StableVersion(version)
-	assetNames := buildAssetNames(version, stable)
+	v, err := version.NewVersion(versionStr)
+	if err != nil {
+		return nil, err
+	}
+	stable := v.Prerelease() == ""
+
+	assetNames := buildAssetNames(versionStr, stable)
 	assets, err := github.DownloadAssetUrl(tag, assetNames, r.conf.TofuRemoteUrl, r.conf.GithubToken)
 	if err != nil {
 		return nil, err
@@ -62,30 +74,8 @@ func (r *TofuRetriever) DownloadReleaseZip(version string) ([]byte, error) {
 		return nil, err
 	}
 
-	dataSums, err := download.DownloadBytes(assets[assetNames[1]])
-	if err != nil {
+	if err = checkSumAndSig(v, stable, data, assetNames, assets, r.conf.Verbose); err != nil {
 		return nil, err
-	}
-
-	if err = sha256check.Check(data, dataSums, assetNames[0]); err != nil {
-		return nil, err
-	}
-
-	if stable {
-		dataSig, err := download.DownloadBytes(assets[assetNames[2]])
-		if err != nil {
-			return nil, err
-		}
-
-		dataPublicKey, err := download.DownloadBytes(publicKeyUrl)
-		if err != nil {
-			return nil, err
-		}
-
-		err = pgpcheck.Check(data, dataSig, dataPublicKey)
-		if err != nil {
-			return nil, err
-		}
 	}
 	return data, nil
 }
@@ -109,16 +99,69 @@ func buildAssetNames(version string, stable bool) []string {
 	nameBuilder.WriteString(".zip")
 	zipAssetName := nameBuilder.String()
 
-	nameBuilder.WriteString(".gpgsig")
-	pgpAssetName := nameBuilder.String()
-
 	nameBuilder.Reset()
 	nameBuilder.WriteString("tofu_")
 	nameBuilder.WriteString(version)
 	nameBuilder.WriteString("_SHA256SUMS")
+	sumsAssetName := nameBuilder.String()
 
 	if stable {
-		return []string{zipAssetName, nameBuilder.String(), pgpAssetName}
+		return []string{zipAssetName, sumsAssetName, sumsAssetName + ".pem", sumsAssetName + ".sig", sumsAssetName + ".gpgsig"}
 	}
-	return []string{zipAssetName, nameBuilder.String()}
+	return []string{zipAssetName, sumsAssetName, sumsAssetName + ".pem", sumsAssetName + ".sig"}
+}
+
+func buildIdentity(v *version.Version) string {
+	cleanedVersion := v.String()
+	indexDot := strings.LastIndexByte(cleanedVersion, '.')
+	// cleaned, so indexDot can not be -1
+	shortVersion := cleanedVersion[:indexDot]
+	return baseIdentity + shortVersion
+}
+
+func checkSumAndSig(v *version.Version, stable bool, data []byte, assetNames []string, assets map[string]string, verbose bool) error {
+	dataSums, err := download.DownloadBytes(assetNames[1])
+	if err != nil {
+		return err
+	}
+
+	if err = sha256check.Check(data, dataSums, assetNames[0]); err != nil {
+		return err
+	}
+
+	dataSumsSig, err := download.DownloadBytes(assetNames[3])
+	if err != nil {
+		return err
+	}
+
+	dataSumsCert, err := download.DownloadBytes(assetNames[2])
+	if err != nil {
+		return err
+	}
+
+	identity := buildIdentity(v)
+	err = cosigncheck.Check(dataSums, dataSumsSig, dataSumsCert, identity, issuer)
+	if err == nil || err != cosigncheck.ErrNotInstalled {
+		return err
+	}
+
+	if stable {
+		if verbose {
+			fmt.Println("cosign executable not found, fallback to pgp check")
+		}
+	} else {
+		fmt.Println("cosign executable not found and pgp check not available for unstable version")
+		return nil
+	}
+
+	dataSumsSig, err = download.DownloadBytes(assetNames[4])
+	if err != nil {
+		return err
+	}
+
+	dataPublicKey, err := download.DownloadBytes(publicKeyUrl)
+	if err != nil {
+		return err
+	}
+	return pgpcheck.Check(dataSums, dataSumsSig, dataPublicKey)
 }
