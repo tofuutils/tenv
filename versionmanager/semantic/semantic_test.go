0ed10e2ace67f333b52833e029f46f32e06fa5a8
Denis Vaumoron <dvaumoron@gmail.com>
2024-01-18 16:42:30 +0100

    add tests to semantic package

---
+++ versionmanager/semantic/semantic_test.go
@@ -0,0 +1,47 @@
+/*
+ *
+ * Copyright 2024 gotofuenv authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package semantic_test
+
+import (
+	"slices"
+	"testing"
+
+	"github.com/dvaumoron/gotofuenv/versionmanager/semantic"
+)
+
+func TestCmpVersion(t *testing.T) {
+	var versions = []string{"1.6.0-beta5", "1.5.2", "1.6.0-alpha5", "1.6.0", "1.5.1", "1.5.0", "1.6.0-rc1"}
+	slices.SortFunc(versions, semantic.CmpVersion)
+	if !slices.Equal(versions, []string{"1.5.0", "1.5.1", "1.5.2", "1.6.0-alpha5", "1.6.0-beta5", "1.6.0-rc1", "1.6.0"}) {
+		t.Error("Unmatching results, get :", versions)
+	}
+}
+
+func TestStableVersion(t *testing.T) {
+	var filtered []string
+	for _, version := range []string{"1.5.0", "1.5.1", "1.5.2", "1.6.0-alpha5", "1.6.0-beta5", "1.6.0-rc1", "1.6.0"} {
+		if semantic.StableVersion(version) {
+			filtered = append(filtered, version)
+		}
+	}
+
+	if !slices.Equal(filtered, []string{"1.5.0", "1.5.1", "1.5.2", "1.6.0"}) {
+		t.Error("Unmatching results, get :", filtered)
+	}
+}
