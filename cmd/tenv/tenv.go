f044025bd21f77007bb2501748c7ca2ad6ad7819
Denis Vaumoron <dvaumoron@gmail.com>
2024-01-23 14:19:39 +0100

    improve version display

--- cmd/tenv/tenv.go
+++ cmd/tenv/tenv.go
@@ -98,7 +98,7 @@ func newVersionCmd() *cobra.Command {
 		Long:  rootVersionHelp,
 		Args:  cobra.NoArgs,
 		Run: func(_ *cobra.Command, _ []string) {
-			fmt.Println("gotofuenv", version)
+			fmt.Println("tenv from GoTofuEnv", version)
 		},
 	}
 }
