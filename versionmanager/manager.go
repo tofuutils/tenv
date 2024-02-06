511ef88ff0f921e2394d75f14c0f510c5c31b79d
Denis Vaumoron <dvaumoron@gmail.com>
2024-02-06 14:54:58 +0100

    improve display

--- versionmanager/manager.go
+++ versionmanager/manager.go
@@ -169,7 +169,7 @@ func (m VersionManager) Resolve(defaultStrategy string) (string, error) {
 	}
 
 	if m.conf.DisplayNormal {
-		fmt.Println("No", m.FolderName, "version found in basic files, fallback to", color.GreenString(defaultStrategy), "strategy") //nolint
+		fmt.Println("No version files found for", m.FolderName, "fallback to", color.GreenString(defaultStrategy), "strategy") //nolint
 	}
 
 	return defaultStrategy, nil
