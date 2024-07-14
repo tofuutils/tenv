268fcf593850fb8c0f68d36134ea81c699fc0504
Denis Vaumoron <dvaumoron@gmail.com>
2024-07-14 18:12:57 +0200

    update uninstall cli help #181
    Signed-off-by: Denis Vaumoron <dvaumoron@gmail.com>

--- cmd/tenv/subcmd.go
+++ cmd/tenv/subcmd.go
@@ -316,6 +316,7 @@ func newUninstallCmd(conf *config.Config, versionManager versionmanager.VersionM
 	descBuilder.WriteString("Uninstall versions of ")
 	descBuilder.WriteString(versionManager.FolderName)
 	descBuilder.WriteString(` (remove them from TENV_ROOT directory).
+
 Without parameter, display a list to select several versions.
 
 If a parameter is passed, available parameter options:
