efb83d1ac48e81b442a91ea46b9f5d061cda74d6
Denis Vaumoron <dvaumoron@gmail.com>
2024-09-08 18:38:53 +0200

    fix lint
    Signed-off-by: Denis Vaumoron <dvaumoron@gmail.com>

--- versionmanager/proxy/proxy.go
+++ versionmanager/proxy/proxy.go
@@ -74,7 +74,7 @@ func ExecPath(installPath string, version string, execName string, displayer log
 
 func updateWorkPath(conf *config.Config, cmdArgs []string) {
 	for _, arg := range cmdArgs {
-		if chdirPath, ok := strings.CutPrefix(arg, chdirFlagPrefix); ok {
+		if chdirPath, ok := strings.CutPrefix(arg, chdirFlagPrefix); ok { //nolint
 			conf.WorkPath = chdirPath
 
 			return
