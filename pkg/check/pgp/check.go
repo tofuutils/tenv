22e45e9e49d807a65434b6b7711a3e4c5f54b559
Nikolai Mishin <sanduku.default@gmail.com>
2026-04-20 00:05:59 +0200

    Fix PGP signature check
    Add support for multiple public PGP keys in the file.
    
    Signed-off-by: Nikolai Mishin <sanduku.default@gmail.com>

--- pkg/check/pgp/check.go
+++ pkg/check/pgp/check.go
@@ -18,21 +18,67 @@
 
 package pgpcheck
 
-import "github.com/ProtonMail/gopenpgp/v2/crypto"
+import (
+	"errors"
+	"strings"
+
+	"github.com/ProtonMail/gopenpgp/v2/crypto"
+)
+
+var (
+	ErrCheck    = errors.New("invalid pgp signature")
+	ErrNoKey    = errors.New("no valid pgp public keys found")
+	ErrKeyBlock = errors.New("invalid pgp key block")
+)
 
 func Check(data []byte, dataSig []byte, dataPublicKey []byte) error {
+	keys, err := extractKeys(dataPublicKey)
+	if err != nil {
+		return err
+	}
+
 	pgpSignature := crypto.NewPGPSignature(dataSig)
-	publicKeyObj, err := crypto.NewKeyFromArmored(string(dataPublicKey))
-	if err != nil {
-		return err
-	}
-
-	signingKeyRing, err := crypto.NewKeyRing(publicKeyObj)
-	if err != nil {
-		return err
-	}
-
 	message := crypto.NewPlainMessage(data)
 
-	return signingKeyRing.VerifyDetached(message, pgpSignature, crypto.GetUnixTime())
+	for _, key := range keys {
+		keyRing, err := crypto.NewKeyRing(key)
+		if err != nil {
+			continue
+		}
+
+		if err = keyRing.VerifyDetached(message, pgpSignature, crypto.GetUnixTime()); err == nil {
+			return nil
+		}
+	}
+
+	return ErrCheck
+}
+
+func extractKeys(dataPublicKey []byte) ([]*crypto.Key, error) {
+	keyStr := string(dataPublicKey)
+	keySep := "-----BEGIN PGP PUBLIC KEY BLOCK-----"
+
+	capacity := strings.Count(keyStr, keySep)
+
+	keys := make([]*crypto.Key, 0, capacity)
+
+	for part := range strings.SplitSeq(keyStr, keySep) {
+		if strings.TrimSpace(part) == "" {
+			continue
+		}
+
+		armoredKey := keySep + part
+		publicKeyObj, err := crypto.NewKeyFromArmored(armoredKey)
+		if err != nil {
+			return nil, ErrKeyBlock
+		}
+
+		keys = append(keys, publicKeyObj)
+	}
+
+	if len(keys) == 0 {
+		return nil, ErrNoKey
+	}
+
+	return keys, nil
 }
